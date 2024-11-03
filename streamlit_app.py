39e71875958a489ba867df8124cc2d1f9a4402bf
redwane-lourhmati <127763712+redwane-lourhmati@users.noreply.github.com>
2024-11-03 17:25:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,20 +48,4 @@ if ingredients_list:
        session.sql(my_insert_stmt).collect()
        st.success('Your smoothie is ordered!', icon="✅")
        st.write(f"✅ Your smoothie is ordered, {name_on_order}!")
-
-import requests
-
-# Attempt to fetch data from the API
-try:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    
-    # Check if the request was successful
-    if fruityvice_response.status_code == 200:
-        st.text(fruityvice_response.json())  # Display the JSON data if successful
-    else:
-        st.text(f"Error: Received a {fruityvice_response.status_code} status code.")
-        st.text(f"Response: {fruityvice_response.text}")
-
-except requests.exceptions.RequestException as e:
-    # Handle any network errors
-    st.text(f"Request failed: {e}")
+    fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
