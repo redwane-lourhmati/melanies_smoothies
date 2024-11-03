49990e6fa0c395b81f938d91dd96d22cec3319e5
redwane-lourhmati <127763712+redwane-lourhmati@users.noreply.github.com>
2024-11-03 17:28:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,6 @@ if ingredients_list:
        session.sql(my_insert_stmt).collect()
        st.success('Your smoothie is ordered!', icon="✅")
        st.write(f"✅ Your smoothie is ordered, {name_on_order}!")
-    fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+       #st.text(fruityvice_response)
+       fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
